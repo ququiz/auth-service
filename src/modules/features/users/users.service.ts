30b0791b1fa7d8c84a7826b8fe3676bbb45ab772
Alexander Adam <alexanderadamm21@gmail.com>
2024-04-05 20:00:17 +0700

    feat(users): implement password hashing on registration

--- src/modules/features/users/users.service.ts
+++ src/modules/features/users/users.service.ts
@@ -4,11 +4,16 @@ import {
   UserRegistrationResDTO,
 } from './dtos/user-registration.dto';
 import { User } from 'src/modules/datasources/entities/users.entity';
+import { hash } from 'argon2';
+import { ConfigService } from '@nestjs/config';
 import { UsersRepository } from 'src/modules/datasources/repositories/users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   public async postUserRegistration(
     payload: UserRegistrationReqBodyDTO,
@@ -24,7 +29,7 @@ export class UsersService {
     newUser.fullname = payload.fullname;
     newUser.email = email;
     newUser.username = username;
-    newUser.password = payload.password;
+    newUser.password = await this.hashPassword(payload.password);
 
     const addedUser = await this.usersRepository.save(newUser);
 
@@ -35,5 +40,11 @@ export class UsersService {
     };
 
     return responseDto;
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return hash(password, {
+      salt: this.configService.get<string>('PASSWORD_SALT'),
+    });
   }
 }
